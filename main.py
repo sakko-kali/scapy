02675fbbc0eaacdfaae78efb9d6e129f718a382c
Sako-kali <sarkisssbarsegian@gmail.com>
2025-08-30 17:26:40 +0400

    second commit

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from scapy.all import sniff
+from scapy.all import *
 import socket
 
 
-name = socket.gethostbyaddr("18.244.87.25")
-def show_packet(packet):
-    print(packet.summary())  # Краткая инфа о пакете
+# name = socket.gethostbyaddr("18.244.87.25")
+# def show_packet(packet):
+#     print(packet.summary())  # Краткая инфа о пакете
+#
+# sniff(count=5, prn=show_packet)
+# print(name)
 
-sniff(count=5, prn=show_packet)
-print(name)
+# packet = IP(dst="8.8.8.8")/ICMP()
+#
+# reply = sr1(packet, timeout=5)
+#
+# if reply:
+#     reply.show()
+# else:
+#     print("-")
