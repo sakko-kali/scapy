9a75b557c349dd41062ab24764304738ff5653e8
Sako-kali <sarkisssbarsegian@gmail.com>
2025-08-30 17:52:20 +0400

    third

--- main.py
+++ main.py
@@ -17,3 +17,12 @@ import socket
 #     reply.show()
 # else:
 #     print("-")
+
+# ips=["8.8.8.8","45.33.32.156","0.0.0.0"]
+# for ip in ips:
+#     ip = IP(dst=ip)
+#     tcp = TCP(dport=80, flags="S")
+#     packet = ip/tcp
+#     reply = sr1(packet, timeout=10)
+#     if reply:
+#         reply.show()
